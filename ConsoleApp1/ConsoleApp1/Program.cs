31a53a5f0bd24e0968ad2ff17d6d99d291e13d31
jadg13 <jadg13@hotmail.com>
2024-08-26 11:49:20 -0600

    Agregué la variable edad

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
+            int edad;
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Estructuras de datos");
+            Console.WriteLine("Digita la edad:");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine($"La edad es {edad}");
         }
     }
 }
